26540fb06ab411905c7734e3fc3cdf249139bf8d
Julian Stieß <j_stiess17@cs.uni-kl.de>
2023-12-21 16:07:18 +0100

    auto reload on loaded failure geil

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ function createWindow() {
     const win  = new BrowserWindow({
         fullscreen: true,
     });
+    console.log(win)
+    win.webContents.on("did-fail-load", function(e) {
+        console.log("Fuck did not load");
+        setTimeout(() => {
+            win.loadURL(MMURL);
+        },5000)
+    });
     win.loadURL(MMURL);
+
 }
 app.whenReady().then(createWindow);
