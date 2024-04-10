363ad62c4ec231e1f527b4348b6fe60a3e9db18c
Julian Stieß <j_stiess17@cs.uni-kl.de>
2024-04-10 21:28:08 +0200

    hm

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 const MMURL = "http://192.168.178.42:9070"
+
 function createWindow() {
-    const win  = new BrowserWindow({
+    const win = new BrowserWindow({
         fullscreen: true,
     });
     console.log(win)
     win.webContents.on("did-fail-load", function(e) {
         console.log("Fuck did not load");
         setTimeout(() => {
-            win.loadURL(MMURL);
-        },5000)
+            win.loadUrl("https://juicyjul.es")
+            createWindow();
+        }, 5000)
     });
     win.loadURL(MMURL);
 
